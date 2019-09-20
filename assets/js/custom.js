96e2d2e01a14ebdf073d1051bd15d290f855406c
Giovanni Max <mindlesh13@gmail.com>
2019-09-20 19:15:00 +0800

    closenav

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,13 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("openNav").style.display = "none";
+  document.getElementById("closeNav").style.display = "flex";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("openNav").style.display = "flex";
+  document.getElementById("closeNav").style.display = "none";
 }
